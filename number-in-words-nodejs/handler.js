8be9bf87bd2ddca828bf3d9a2ea87f827c00e0eb
Evandro Pires da Silva <evandro@MacBook-Pro-de-Evandro.local>
2018-10-05 22:57:32 -0300

    adicionando serverless e nodejs

---
+++ number-in-words-nodejs/handler.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var num2words1 = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+            6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+            11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
+            15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'};
+var num2words2 = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+function main(dict) {
+    var num = Number(dict['number']);
+    return { 'inWords': number(num) };
+}
+function number(Number) {
+    if (1 <= Number < 19) {
+        return num2words1[Number];
+    } else if (20 <= Number <= 99) {
+        tens, below_ten = divmod(Number, 10);
+        return num2words2[tens - 2] + '-' + num2words1[below_ten];
+    } else {
+        console.log('Number out of range');
+
+    }
+}
